32fb0c2137c5d3a6ba719fe671681cd273cb2805
Orst547 <urielpes.ups@gmail.com>
2024-07-19 11:30:08 -0600

    eliminado cosas que no sirven

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import Especialidad from "./components/Especialidad";
 import Registro from "./components/Registro";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import Navigation from "./layouts/Navigation";
 import Citas from "./components/Citas";
 import 'primeicons/primeicons.css';
-import Especialidades from "./components/Especialidades";
 import VerCitas from "./components/VerCitas";
         
 
@@ -23,8 +21,6 @@ function App() {
         <Route path="/" Component={Login}></Route>
         <Route path="/registro" Component={Registro}></Route>
           <Route path="/home" Component={Home}></Route>
-          <Route path="/especialidades" Component={Especialidades}></Route>
-          <Route path="/especialidad/:id?" Component={Especialidad}></Route>
           <Route path="/nueva_cita" Component={Citas}></Route>
           <Route path="/ver_citas" Component={VerCitas}></Route>
         </Routes>
--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,15 +1,15 @@
 // Navbar.js
 
-import React from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
-import { Navbar, Nav, Container, NavDropdown, Button } from 'react-bootstrap';
-import '../css/Navbar.css'; // Archivo CSS para estilos personalizados
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import "../css/Navbar.css"; // Archivo CSS para estilos personalizados
 
 function Navigation() {
   const location = useLocation();
 
   // Lista de rutas donde no queremos mostrar la navbar
-  const hideNavbarRoutes = ['/', '/registro'];
+  const hideNavbarRoutes = ["/", "/registro"];
 
   // Verifica si la ruta actual está en la lista de rutas ocultas
   const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
@@ -17,32 +17,33 @@ function Navigation() {
   if (shouldHideNavbar) {
     return null; // No renderizar la navbar si la ruta está en la lista de rutas ocultas
   }
+  const handleLogout = () => {
+
+    localStorage.clear();
+  };
 
   return (
     <Navbar expand="lg" className="custom-navbar">
       <Container>
-        <Navbar.Brand as={NavLink} to="/home" className="custom-brand">UTTEC</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/home" className="custom-brand">
+          UTTEC
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/especialidades">
-              Especialidad
+            <Nav.Link as={NavLink} to="/nueva_cita">
+              Generar cita
             </Nav.Link>
-
-            <NavDropdown title="Citas" id="basic-nav-dropdown">
-              <NavDropdown.Item as={NavLink} to="/nueva_cita">Generar cita</NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/ver_citas">Ver mis citas</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={NavLink} to="/ver_citas">
+            Ver mis citas
+            </Nav.Link>
           </Nav>
           <Nav className="ml-auto">
             <NavDropdown title="Cuenta" id="account-nav-dropdown" align="end">
               <NavDropdown.Item as={NavLink} to="/">
                 Login
               </NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/logout">
+              <NavDropdown.Item as={NavLink} to='/'  onClick={handleLogout}>
                 Logout
               </NavDropdown.Item>
             </NavDropdown>
